97d44effdad9655525f0e124b5ce9117a8c1cdec
Alexandru Tofănel <237447640+talexbo@users.noreply.github.com>
2025-12-09 16:42:51 +0200

    Create etaj.cpp

---
+++ tema_3/etaj.cpp
@@ -0,0 +1,6 @@
+//Un anumit etaj al unei clădiri este reprezentat schematic sub forma unei matrice
+//ce conţine valorile -1 şi 0, unde -1 reprezintă zid şi 0 reprezintă spaţiu
+//liber. Pereţii sunt de grosime 1 şi uşile nu sunt marcate (se consideră tot
+//perete). Care perete poate fi dărâmat (o poziţie maractă cu -1 se va marca
+//cu 0) a. î. să se obţină o încăpere de suprafaţă maximă? Evitaţi utilizarea
+//recursivităţii. Puteti folosi queue din C++.
