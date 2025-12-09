41513a9a0db21cf2cf345f06afad078597749a27
Alexandru Tofănel <237447640+talexbo@users.noreply.github.com>
2025-12-09 16:45:54 +0200

    Create paranteze.cpp

---
+++ tema_3/paranteze.cpp
@@ -0,0 +1,4 @@
+//Se dă un şir de paranteze deschise şi închise de tip (, ), [, ], {, }. Să se
+//verifice dacă şirul este corect. Pentru rezolvare folosiţi o stivă de catactere
+//implementare proprie sau std::stack. Exemplu: şirul [()()] este corect, şirul
+//([]) nu este corect, şirul ()]( nu este corect.
