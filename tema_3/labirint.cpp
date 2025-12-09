49d73cb3cea89105f736d6e914a92820299adb1c
Alexandru Tofănel <237447640+talexbo@users.noreply.github.com>
2025-12-09 16:40:33 +0200

    Add maze solving algorithm in labirint.cpp
    Implement maze solving algorithm using a queue.

---
+++ tema_3/labirint.cpp
@@ -0,0 +1,5 @@
+// 12. Se consideră un labirint reprezentat printr-o matrice în care zidurile sunt marcate
+// prin −1 şi drumurile prin 0. În labirint se află un şoricel pe poziţia (x0 , y0 )
+// şi o bucată de brânză pe poziţia (x1 , y1 ). Să se găsească un drum (de preferinţă
+// de lungime minimă) de la şoricel la brânză. Nu utilizaţi recursivitate! Utilizaţi
+// o coadă. Puteti folosi queue din C++.
